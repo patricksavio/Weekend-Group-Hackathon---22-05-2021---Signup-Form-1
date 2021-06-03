958fc8f394ea22c4ff7aec556ba4acdcbbecc142
Rekha-1 <maneeta808@gmail.com>
2021-06-03 16:25:18 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState,useEffect } from "react";
+import Form from './Form';
 import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <div className="row">
+        <div className="col-md-12 col-12">
+        <Form/>
+        </div>
+      </div>
+    </div>
   )
 }
 
